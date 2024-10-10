357ae76051f79bfe23214826f356f9331d3d0106
Pawandeepkaur05 <120079621+Pawandeepkaur05@users.noreply.github.com>
2024-10-10 14:19:08 -0600

    updating

--- app/component/header.js
+++ app/component/header.js
@@ -7,7 +7,7 @@ import styles from "../cssFiles/header.module.css";// adjust the styles or path 
 import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+
 
 const Header = () => {
   const router = useRouter();
